183de03a6733c75266dba81dfd5a5b4f233721ae
Alain Wittwer <wittwer.alain@gmail.com>
2019-09-08 13:01:47 +0200

    implement HomeComponent

--- crash-course/src/app/app.module.ts
+++ crash-course/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ crash-course/src/app/home/home.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  clickCounter: number = 0;
+  name: string = 'Alain is cool';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  countClick() {
+    this.clickCounter += 1;
+  }
+
+  setClasses() {
+    let myClasses = {
+      active: this.clickCounter > 4,
+      notactive: this.clickCounter <= 4,
+    }
+    return myClasses;
+  }
+
+}
