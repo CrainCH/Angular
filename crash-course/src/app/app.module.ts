a6ff3532943c6ce8ea32f3d0efc03cb9f065ba0b
Alain Wittwer <wittwer.alain@gmail.com>
2019-09-08 13:33:33 +0200

    implement ListComponent

--- crash-course/src/app/app.module.ts
+++ crash-course/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,6 +18,7 @@ import { ListComponent } from './list/list.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
