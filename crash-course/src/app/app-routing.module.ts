eb226f15683be069bcf7226a66feaa96c24baf45
Alain Wittwer <wittwer.alain@gmail.com>
2019-09-08 22:22:12 +0200

    fix routes

--- crash-course/src/app/app-routing.module.ts
+++ crash-course/src/app/app-routing.module.ts
@@ -4,8 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { ListComponent } from './list/list.component';
 
 const routes: Routes = [
-  { path: '', component: ListComponent },
-  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
   { path: 'list', component: ListComponent },
 ];
 
