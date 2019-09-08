997357ff059ba94b6d6afaaa051c443e4ee0c306
Alain Wittwer <wittwer.alain@gmail.com>
2019-09-08 22:05:57 +0200

    add BrewAdapter

--- crash-course/src/app/http.service.ts
+++ crash-course/src/app/http.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Brew } from './list/Brew';
+import { Brew, BrewAdapter } from './list/Brew';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
@@ -9,25 +9,17 @@ import { map } from 'rxjs/operators';
 })
 export class HttpService {
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private adapter: BrewAdapter,
+  ) { }
 
   getBeer(): Observable<Brew[]> {
     const url = 'https://api.openbrewerydb.org/breweries';
     return this.http.get(url).pipe(
-      map((data: any[]) => data.map((item: any) => new Brew(
-        item.id,
-        item.name,
-        item.brewery_type,
-        item.street,
-        item.city,
-        item.state,
-        item.postal_code,
-        item.country,
-        item.longitude,
-        item.latitude,
-        item.phone,
-        item.website_url,
-      ))),
+      // Adapt each item in the raw data array
+      map((data: any[]) => data.map(item => this.adapter.adapt(item))),
     );
   }
+
 }
--- crash-course/src/app/list/Brew.ts
+++ crash-course/src/app/list/Brew.ts
@@ -1,3 +1,6 @@
+import { Injectable } from '@angular/core';
+import { Adapter } from './Adapter';
+
 export class Brew {
     constructor(
         public id: number,
@@ -13,4 +16,28 @@ export class Brew {
         public phone: string,
         public website_url: string,
     ) { }
+}
+
+@Injectable({
+    providedIn: 'root'
+})
+export class BrewAdapter implements Adapter<Brew> {
+
+    adapt(item: any): Brew {
+        return new Brew(
+            item.id,
+            item.name,
+            item.brewery_type,
+            item.street,
+            item.city,
+            item.state,
+            item.postal_code,
+            item.country,
+            item.longitude,
+            item.latitude,
+            item.phone,
+            item.website_url,
+        );
+    }
+
 }
